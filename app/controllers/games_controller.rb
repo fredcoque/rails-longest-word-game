798ef3eae6b1a68f918a1fce8c7550418a6d691e
Fred Coque <fredcoque@yahoo.com>
2020-10-10 09:03:16 -0500

    Undone

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+  def new
+    @grid = generate_grid(9)
+    @start_time = Time.now
+  end
+  def score
+  end
+  def generate_grid(grid_size)
+    return ('A'..'Z').to_a.sample(grid_size).join(' ')
+  end
 end
